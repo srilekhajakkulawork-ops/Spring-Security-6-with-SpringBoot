c3ee1b03e3154718dc43fb555af231c5c337884f
Srilekha Jakkula <srilekhajakkula.work@gmail.com>
2025-10-25 21:59:51 -0500

    Initial commit: Spring Boot OAuth2 project

---
+++ src/main/java/com/example/Oauth/PracspringbootOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.Oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracspringbootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracspringbootOauth2Application.class, args);
+	}
+
+}
